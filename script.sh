c0c4df7473e11f374f1626382e6d7ac0ff5d62b5
Raghavi Shirur <raghaviss11@gmail.com>
2022-04-27 14:23:37 +0530

    changes made to script2

--- script.sh
+++ script.sh
@@ -19,16 +19,18 @@ then
 else
   kubectl create namespace argocd
 fi
+
 kubectl apply -n argocd -f manifests/argo.yaml 
 kubectl apply -n kube-system -f manifests/components.yaml
 
 tput setaf 3; echo "Waiting for ArgoCD to start . . ." ; tput sgr0
-
+set +e
 Resources=("argocd-application-controller" "argocd-applicationset-controller" "argocd-dex-server" "argocd-notifications-controller" "argocd-redis" "argocd-repo-server" "argocd-server")
 for resource in ${Resources[*]}; do
     kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=$resource -n argocd
 done
 
+set -e
 tput setaf 2; echo "Creating application . . ." ; tput sgr0
 
 kubectl apply -n argocd -f ./application.yaml
@@ -36,6 +38,7 @@ kubectl apply -n argocd -f ./application.yaml
 tput setaf 6; echo "Loading metrics . . ."; tput sgr0
 sleep 200
 tput setaf 6; echo "Error statements regarding metrics unavailability can be ignored"; tput sgr0
+set +e
 while true; do kubectl top pods -n argocd; tput setaf 6; date; tput sgr0; sleep 20; done;
 
 
